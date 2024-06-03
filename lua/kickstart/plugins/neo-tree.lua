21bb456fc63136fde3ef2f95150ca338190bf199
Stefan Cernescu <stefan.cernescu@bp.com>
2024-06-03 20:22:38 +0300

    Add additional nvim plugins (Debug, auto session, trouble

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -30,4 +30,37 @@ return {
       },
     },
   },
+  window = {
+    mappings = {
+      ['<space>'] = 'none',
+      ['Y'] = {
+        function(state)
+          local node = state.tree:get_node()
+          local path = node:get_id()
+          vim.fn.setreg('+', path, 'c')
+        end,
+        desc = 'Copy Path to Clipboard',
+      },
+      ['O'] = {
+        function(state)
+          require('lazy.util').open(state.tree:get_node().path, { system = true })
+        end,
+        desc = 'Open with System Application',
+      },
+    },
+  },
+  default_component_configs = {
+    indent = {
+      with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+      expander_collapsed = '',
+      expander_expanded = '',
+      expander_highlight = 'NeoTreeExpander',
+    },
+    git_status = {
+      symbols = {
+        unstaged = '󰄱',
+        staged = '󰱒',
+      },
+    },
+  },
 }
